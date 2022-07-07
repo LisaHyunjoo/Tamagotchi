86976ab116be2c08e89ef8b2abeda7603e2f2978
Lisa <lisahongofficial@gmail.com>
2022-07-07 10:53:54 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+// game class for Tamagotchi,
+
+// Instantiate Tamagotchi 
+
+//clicking howtoplay tag => 
+//game instructions display on webpage. 
+
+//press start button => display a character.
+//Morph your pet at certain ages.
+
+// ask pet's name 
+// Input pet's name => Count {
+    // Age
+    // Hunger (1-10 scale)
+    // Sleepiness (1-10 scale)
+    // Boredom (1-10 scale)
+//} 
+
+
+// Increase pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+
+// Add buttons to the screen to feed pet, turn off the lights, and play with  pet.
+// one click each btn => count -1 the scale
+
+// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
