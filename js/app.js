ee920df2e4c502c7c42f2e95b8e4ff067a80aaf6
Lisa <lisahongofficial@gmail.com>
2022-07-08 23:23:18 -0400

    class for Tamagotchi

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
 
 // game class for Tamagotchi,
-
+class Tamagotchi {
+    constructor (name, hunger, sleepiness, boredom, age) {
+        this.name = name;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
+        this.age = age;
+    }
+}
 // Instantiate Tamagotchi 
+class Pet extends Tamagotchi {
+    constructor (name, hunger, sleepiness, boredom, age) {
+        super(name, hunger, sleepiness, boredom, age)
+        this.name = name;
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+        this.age = 0;
+    }
+}
+const doggy = new Pet();
 
 //clicking howtoplay tag => 
 //game instructions display on webpage. 
